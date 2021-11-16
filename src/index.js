48b92a12aa7dbdfe60b01456876bfeed314992de
angrythunderbird <headcrab.albert@gmail.com>
2021-11-16 20:58:33 +0200

    feat: add fetch request

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import './index.scss';
 
 import header from './Header';
 import { form, inputEmail, inputPass } from './Form-group';
-
-// import InputsUI from './config/UI.config';
 import { validate } from './helpers/validate';
 import { showInputError, removeInputError } from './popups';
+import { login } from './services/auth.service';
 
 const rootEl = document.getElementById('root');
 const inputs = [inputEmail, inputPass];
@@ -21,17 +20,10 @@ form.addEventListener('submit', e => {
   onSubmit();
 })
 
-// inputs.forEach(el => el.addEventListener('change', () => {
-//   inputs.forEach(input => {
-//     (!validate(input)) ? showInputError(input) : removeInputError(input);
-//   })
-//   removeInputError(el)
-// }));
-
 inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
 
 // Handlers
-function onSubmit() {
+async function onSubmit() {
   const isValidForm = inputs.every(el => {
     const isValidInput = validate(el);
     if (!isValidInput) {
@@ -41,4 +33,18 @@ function onSubmit() {
     }
     return isValidInput;
   })
+
+  if (!isValidForm) {
+    alert('wrong login data');
+    return;
+  }
+
+  // denis.m.pcspace@gmail.com, dmgame12345
+  try {
+    await login(inputs[0].value, inputs[1].value);
+    form.reset();
+    // show success notify
+  } catch (error) {
+    // show success notify
+  }
 }
